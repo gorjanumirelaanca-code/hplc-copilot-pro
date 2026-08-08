1b998c0faf5d7332873b468100868a30139591c9
Mirela G <gorjanu.mirela@yahoo.ca>
2026-08-07 18:45:30 -0700

    Fix Prisma client for Vercel build

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 
 const globalForPrisma = globalThis as unknown as {
@@ -7,16 +6,9 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 
-const adapter = new PrismaBetterSqlite3({
-  url: "file:./dev.db",
-});
-
-
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter,
-  });
+  new PrismaClient();
 
 
 if (process.env.NODE_ENV !== "production") {
