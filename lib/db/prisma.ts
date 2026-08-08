312d44d745314a93a195ad072b010169901b72bc
Mirela G <gorjanu.mirela@yahoo.ca>
2026-08-07 19:28:32 -0700

    Fix Prisma 7 and Next.js build configuration

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,12 +1,23 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./dev.db",
+});
+
+
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient();
+  new PrismaClient({
+    adapter,
+  });
+
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
